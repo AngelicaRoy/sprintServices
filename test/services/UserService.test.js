ee452d10b119fd43b3505420087f2c8974fa7e34
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-25 18:37:47 -0500

    Modifico la url de UserService test y la cla clase

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -4,8 +4,8 @@ describe("Tests for UserService", () => {
     test("1. Create a new user using the UserService", () => {
 
         const user=UserService.create(1,"angelicaroy", "Angelica")
-        expect(user.username).toBe("AngelicaRoy")
-        expect(user.name).toBe("angelicaroy")
+        expect(user.username).toBe("angelicaroy")
+        expect(user.name).toBe("Angelica")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
