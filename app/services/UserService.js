54462cfb38cfc448d0d8dff8abbbc52551741e5f
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-25 14:34:19 -0500

    Crear clase UserService.js y prueba UserService.test.js

---
+++ app/services/UserService.js
@@ -0,0 +1,9 @@
+const User=require('./../models/user')
+
+class UserService{
+    static created(id, username, name){
+        return new User(id, username, name, "Sin bio")
+    }
+}
+
+module.exports=UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -4,9 +4,34 @@ describe("Tests for UserService", () => {
     
     test("1. Create a new user using the UserService", ()=>{
         const user=UserService.create(1,"angelicaroy", "Angelica")
-        expect(user.username).toBe(1,"AngelicaRoy","Angelica")
+        expect(user.username).toBe(1,"AngelicaRoy")
         expect(user.name).toBe("angelicaroy")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
-    });
+    })
+
+    test("2. Get all user data in list", () =>{
+        const user=UserService.create(1, "angelicaroy", "Angelica")
+        const userInfoInList=UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1].toBe("angelicaroy"))
+        expect(userInfoInList[2].toBe("Angelica"))
+        expect(userInfoInList[3].toBe("Sin bio"))
+    })  
+
+    test("3. Update username", () => {
+        const user=UserService.create(1, "angelicaroy", "Angelica")
+        UserService.updateUserUsername(user, "angelog")
+        expect(user.username).toBe("angelog")
+    })
+
+    test("4. Given a list of users give me the list of usernames", () => {
+        const user1 =UserService.create(1, "angelicaroy1", "Angelica")
+        const user2 =UserService.create(1, "angelicaroy2", "Angelica")
+        const user3 =UserService.create(1, "angelicaroy3", "Angelica")
+        const usernames=UserService.getAllUsernames([user1, user2, user3])
+        expect(usernames).toContain("angelicaroy1")
+        expect(usernames).toContain("angelicaroy2")
+        expect(usernames).toContain("angelicaroy3")
+    })
 })
