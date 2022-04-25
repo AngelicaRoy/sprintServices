a75d6e1ab2123f4df38332b4e25ad34bb6e535e5
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-25 18:08:12 -0500

    Agregar cambios a UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,7 @@
-const User=require('app/services/UserService')
+const User=require('../../app/services/UserService')
 
 class UserService{
-    static created(id, username, name){
+    static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
 }
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -4,7 +4,7 @@ describe("Tests for UserService", () => {
     test("1. Create a new user using the UserService", () => {
 
         const user=UserService.create(1,"angelicaroy", "Angelica")
-        expect(user.username).toBe(1,"AngelicaRoy")
+        expect(user.username).toBe("AngelicaRoy")
         expect(user.name).toBe("angelicaroy")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
