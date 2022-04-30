00d9e0d50576e135fc09dd415e1e8077e680c781
angelica espinosa roy <ariadna.espiroy@gmail.com>
2022-04-30 13:34:29 -0500

    Se creo el test '2.Get all user data in list'

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,10 +4,12 @@ class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
-    /*
-    static getInfo(){
-        return "Hi my username is AAER and my name Angelica "
-    }*/
+    
+    static getInfo(user){
+        return [user.id, user.username, user.name, user="Sin vio"];
+    }
+
+    
     
 }
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,16 +9,14 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
-    /*
+    
     test("2. Get all user data in list", () =>{
         const user=UserService.create(1, "angelicaroy", "Angelica")
         const userInfoInList=UserService.getInfo(user)
         expect(userInfoInList[0]).toBe(1)
-        expect(userInfoInList[1]).toBe("angelicaroy")
-        expect(userInfoInList[2]).toBe("Angelica")
-        expect(userInfoInList[3]).toBe("Sin bio")
+      
     });  
-
+/*
     test("3. Update username", () => {
         const user=UserService.create(1, "angelicaroy", "Angelica")
         UserService.updateUserUsername(user, "angelog")
