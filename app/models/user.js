19899f9f9553784fed9e2609b1b34b57c79937e9
angelica espinosa roy <ariadna.espiroy@gmail.com>
2022-04-30 13:05:30 -0500

    Crecion de clase User con su constructor

--- app/models/user.js
+++ app/models/user.js
@@ -1,18 +1,10 @@
 class User{
-
     constructor(id, username, name, bio){
         this.id=id
         this.username=username
         this.name=name
         this.bio=bio
-        this.dateCreated=new Date();
-        this.lastUpdated=new Date();
-    }
-
-    
-
-    getInfo(){
-        return "Hey! my name is Angelica and my username is AAER"
     }
 }
+
 module.exports=User
