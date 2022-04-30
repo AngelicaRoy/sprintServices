12894118f1d5b3d69a1dddc095c09558beaefcef
angelica espinosa roy <ariadna.espiroy@gmail.com>
2022-04-30 13:07:30 -0500

    Se creó la clase UserService y dentro de ella el metódo estático create

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,13 +1,13 @@
-const User=require('./../models/user')
+const User=require('./../../app/models/user')
 
 class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
-
+    /*
     static getInfo(){
         return "Hi my username is AAER and my name Angelica "
-    }
+    }*/
     
 }
 
